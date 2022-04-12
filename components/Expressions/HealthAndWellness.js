0e533b0e2cd6ccbfebe0cc02ca72697a8c0d31a4
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-04-12 20:14:08 +0100

    Completed Message Wrapper in HealthAndWellnessComponent

--- components/Expressions/HealthAndWellness.js
+++ components/Expressions/HealthAndWellness.js
@@ -1,12 +1,58 @@
-import React from 'react'
-import classes from "./HealthAndWellness.module.css"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import classes from "./HealthAndWellness.module.css";
 
 const HealthAndWellness = () => {
   return (
-    <div className={classes.HAW}> {/* HAW - Health and Wellness */}
-      
+    <div className={classes.HAW}>
+      {" "}
+      {/* HAW - Health and Wellness */}
+      <div className={classes.title}>
+        <h2>Health &and; Wellness</h2>
+        <h3>
+          I strive to live in communities with stronger, healthier, and happier
+          humans.
+        </h3>
+      </div>
+      <h4 className={classes["sub-title"]}>
+        Fitness is no longer a nice- to- have, but of utmost necessity for long
+        and healthy living.{" "}
+      </h4>
+      <div className={classes["message-wrapper"]}>
+        <div className={classes["message-item"]}>
+          <div className={classes["message-item_image-wrapper"]}>
+            <Image
+              src="/images/fit-1.jpg"
+              alt="Ijoda Fitness and Wellness by Kaffy"
+              width="5568"
+              height="3712"
+              layout="responsive"
+              className={classes["message-item_image"]}
+            />
+            <p className={classes["message-item_image-ref"]}>REF</p>
+          </div>
+            <p className={classes["message-text"]}>
+            Through <Link href={"/"}><a className={classes["ijoda-link"]}>Ijoda Fitness & Wellness,</a></Link>  KaffyI creates fitness solutions that are customised and best suited for a wide range of clientelle. Whether you are trying to treat a health condition, improve body fitness & wellbeing, or experience soul therapy, Kaffy understands the art and science of fitness - and can work with you as a coach, instructor, and consultant.
+            </p>
+        </div>
+        <div className={classes["message-item"]}>
+          <div className={classes["message-item_image-wrapper"]}>
+            <Image
+              src="/images/fit-3.jpg"
+              alt="Ijoda Fitness and Wellness by Kaffy"
+              width="5760"
+              height="3561"
+              layout="responsive"
+              className={classes["message-item_image"]}
+            />
+            <p className={classes["message-item_image-ref"]}>REF</p>
+          </div>
+          <p className={classes["message-text"]}>From A-List celebrities to women who desire to reconnect with their bodies, from High Networth Individuals NIs to middle-class working professionals, Kaffy works with individuals to improve their health - using dance as a tool for fitness and wellness</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HealthAndWellness
+export default HealthAndWellness;
